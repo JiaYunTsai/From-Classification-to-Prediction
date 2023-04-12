112e27e23f88531fe644a84528bdefec89cc2359
Jasmine <jasmine.tsai@junyiacademy.org>
2023-04-13 01:52:08 +0800

    update_similarity_scores

--- AIDA-SC/similarity_scores.py
+++ AIDA-SC/similarity_scores.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 import numpy as np
 import csv
@@ -18,11 +20,11 @@ def up_down_data_split(stock_df, df, keywords):
     df = pd.merge(df, stock_df[['年月日', '漲跌']], on='年月日', how='left')
     df = df.reset_index(drop=True)
 
-    df_up = df.loc[df['漲跌'] == '上漲']
-    df_down = df.loc[df['漲跌'] == '下跌']
+    df_up = df.loc[df['漲跌'] == 'upward_trend']
+    df_down = df.loc[df['漲跌'] == 'downward_trend']
 
-    print('上漲', df_up.shape)
-    print('下跌', df_down.shape)
+    print('Number of upward trend articles', df_up.shape)
+    print('Number of downward trend articles', df_down.shape)
 
     df_up.to_csv('bda2023_mid_dataset/df_up.csv', encoding='utf_8_sig' , index=False)
     df_down.to_csv('bda2023_mid_dataset/df_down.csv', encoding='utf_8_sig' , index=False)
@@ -36,55 +38,69 @@ def monpa_split(query):
     return cut_text
 
 def get_content_list(df):
-    data = np.array(df)
+    content_set = {'upward_trend': [], 'downward_trend': []}
+    df_up = df.loc[df['漲跌'] == 'upward_trend']
+    df_down = df.loc[df['漲跌'] == 'downward_trend']
 
-    content_list = []
-    for row in data:
-        query = f"{row[5]} {row[7]}"
-        cut_text = monpa_split(query)
-        content_list.append(cut_text)
+    for df in [df_up, df_down]:
+        content_list = []
+        for index, row in df.iterrows():
+            query = f"{row['title']} {row['content']}"
+            cut_text = monpa_split(query)
+            content_list.append(cut_text)
+        if df.equals(df_up):
+            content_set['upward_trend'] = content_list
+        else:
+            content_set['downward_trend'] = content_list
 
-    return content_list
+    return content_set
 
-def get_tf_counter_df(df, up_down):
+def get_tf_counter_df(content_set, up_down):
 
-    cut_text_list = get_content_list(df)
-    
-    cut_text_all = ' '.join(cut_text_list)
-    cut_text_all_list = cut_text_all.split(" ")
+    up_list = ' '.join(content_set['upward_trend']).split(" ")
+    down_list = ' '.join(content_set['downward_trend']).split(" ")
 
-    tf_counter=Counter(cut_text_all_list)
+    common_word_list = list(set(up_list).intersection(set(down_list)))
 
-    tf_counter = pd.DataFrame(tf_counter.most_common(100), columns=['term', 'count'])
+    up_list = [word for word in up_list if word not in common_word_list]
+    down_list = [word for word in down_list if word not in common_word_list]
+
+    up_tf_counter = Counter(up_list).most_common(100)
+    down_tf_counter = Counter(down_list).most_common(100)
+
+    print(f"Top 10 up word list{ up_tf_counter[:10]}\n")
+    print(f"Top 10 down word list{ down_tf_counter[:10]}\n")
+
+    tf_counter = down_tf_counter + up_tf_counter
+
+    tf_counter = pd.DataFrame(tf_counter, columns=['term', 'count'])
     tf_counter.to_csv(f'bda2023_mid_dataset/limit_{up_down}_term.csv', encoding='utf_8_sig' , index=False)
     return tf_counter
 
 def get_vocab_id_dict(tf_counter):
-    vocab_id_dict = {} # tf_counter 前面要把沒鑑別力的字扣除
-    #把df的 term 欄位的內容轉成list
+    vocab_id_dict = {} 
     term_list = tf_counter['term'].tolist()
     for i, word in enumerate(term_list):
         vocab_id_dict[word] = i
+
     return vocab_id_dict
 
-
-def get_idf_limit_model(df_up, df_down, content_list, query):
-    df = pd.concat([df_up, df_down], axis=0, ignore_index=True)
-    
+def get_idf_limit_model(df, content_set, query):
     vocab_id_dict = get_vocab_id_dict(df)
-    print(vocab_id_dict)
+    # vocab_id_dict = {'this': 0, 'is': 1, 'a': 2, 'sample': 3, 'sentence': 4}
+    # content_list = [    'This is a sample sentence.',    'This is another example sentence.',    'Here is a third sentence for testing.']
     
     vectorizer = TfidfVectorizer(vocabulary= vocab_id_dict ,use_idf=True)
+    content_list = content_set['upward_trend'] + content_set['downward_trend']
     X = vectorizer.fit_transform(content_list)
-    # print(X)
 
     sparse.save_npz("tmp/limit_model.npz", X)
-
     cut_text = monpa_split(query)
     q =vectorizer.fit_transform([cut_text])
-    print(q[0:1])
+
     cosine_similarities = linear_kernel(q[0:1], X).flatten() #與給定文件集的向量做相似度計算
-    print(cosine_similarities) #印出與每一篇的相似度做觀察
+    related_docs_indices = cosine_similarities.argsort() #將相似度由小至大做排序，並轉換成文件編號
+    print(related_docs_indices)
 
 def main():
     stock_df = pd.read_csv('bda2023_mid_dataset/stock_data_2019_2023_filter_clean.csv')
@@ -98,20 +114,19 @@ def main():
     # keywords = "台達電|2308|電子零組件|汽車電子|電源管理|散熱|電源供應器|乾坤科技|晶睿通訊|力林科技光寶科|群電|康舒"
 
     df_up, df_down = up_down_data_split(stock_df,df_merged, keywords)
-    print("data split done")
+    print("data split done!\n")
 
     df = pd.concat([df_up.head(10), df_down.head(10)], axis=0, ignore_index=True)
-    content_list = get_content_list(df)    
+    content_set = get_content_list(df)    
 
-    tf_counter_df_up = get_tf_counter_df(df_up.head(10),"up")
-    tf_counter_df_down = get_tf_counter_df(df_down.head(10), "down")
-    print("tf_counder done")
+    tf_counter_df = get_tf_counter_df(content_set,"none")
+    print("tf_counder done!!!\n")
  
     query='台股加權指數在最近9個交易日，從最高到最低點，跌了2,544點，創下史上最快速的失速列車紀錄；\
     12日台股盤中急挫1,418點，市場衰鴻遍野，據統計，盤中最多曾有711檔個股觸及跌停、占上市櫃的四成比重，\
     最後仍有251檔收跌停，其中，陽明等15檔股價亮燈跌停，仍有7千張以上賣單高掛，貨櫃三雄均入榜。'
 
-    get_idf_limit_model(tf_counter_df_up, tf_counter_df_down, content_list, query)
+    get_idf_limit_model(tf_counter_df, content_set, query)
     print("idf_limit done")
     
 if __name__ == "__main__":
--- AIDA-SC/try.py
+++
@@ -1,4 +0,0 @@
-cut_text_list = ["1","2", "3"]
-
-cut_text_all = ' '.join(cut_text_list)
-print(type(cut_text_all))
